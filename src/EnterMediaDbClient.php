c3e935d0eb31b62e62ba4ba5bc70fc8e4304ef93
Adam Bramley <adamcbramley@gmail.com>
2016-01-29 12:10:03 +1100

    Add skeleton EnterMediaDbClient service.

---
+++ src/EnterMediaDbClient.php
@@ -0,0 +1,69 @@
+<?php
+/**
+ * @file
+ * Contains \Drupal|embridge\EnterMediaDbClient.
+ */
+
+namespace Drupal\embridge;
+
+
+use Drupal\Component\Serialization\SerializationInterface;
+use Drupal\Core\Config\ConfigFactoryInterface;
+use GuzzleHttp\ClientInterface;
+use GuzzleHttp\Psr7\Request;
+
+/**
+ * Class EnterMediaDbClient.
+ *
+ * @package Drupal\embridge
+ */
+class EnterMediaDbClient {
+
+  /**
+   * Config Factory.
+   *
+   * @var \Drupal\Core\Config\ConfigFactoryInterface
+   */
+  protected $configFactory;
+
+  /**
+   * Client service.
+   *
+   * @var \GuzzleHttp\ClientInterface
+   */
+  protected $httpClient;
+
+  /**
+   * JSON Encoder.
+   *
+   * @var \Drupal\Component\Serialization\SerializationInterface
+   */
+  protected $jsonEncoder;
+
+  /**
+   * Constructs a new \Drupal\entity_pilot\Transport object.
+   *
+   * @param \Drupal\Core\Config\ConfigFactoryInterface $config_factory
+   *   The config factory.
+   * @param \GuzzleHttp\ClientInterface $client
+   *   The http client service.
+   * @param \Drupal\Component\Serialization\SerializationInterface $serializer
+   *   The json serializer service.
+   */
+  public function __construct(ConfigFactoryInterface $config_factory, ClientInterface $client, SerializationInterface $serializer) {
+    $this->configFactory = $config_factory;
+    $this->jsonEncoder = $serializer;
+    $this->httpClient = $client;
+  }
+
+  /**
+   * Initialises a Request object with the configuration.
+   *
+   * @return \GuzzleHttp\Psr7\Request
+   */
+  private function initRequest() {
+    $request = new Request();
+
+    return $request;
+  }
+}
