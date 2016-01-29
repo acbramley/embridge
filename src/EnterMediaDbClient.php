c0c655f17eef0701120426859890cde48034b8e4
Adam Bramley <adamcbramley@gmail.com>
2016-01-29 14:47:01 +1100

    Implement login.

--- src/EnterMediaDbClient.php
+++ src/EnterMediaDbClient.php
@@ -8,13 +8,15 @@ namespace Drupal\embridge;
 
 
 use Drupal\Component\Serialization\SerializationInterface;
+use Drupal\Component\Utility\UrlHelper;
 use Drupal\Core\Config\ConfigFactoryInterface;
 use GuzzleHttp\ClientInterface;
+use GuzzleHttp\Exception\RequestException;
 use GuzzleHttp\Psr7\Request;
 
 class EnterMediaDbClient implements EnterMediaDbClientInterface {
 
-  const EMBRIDGE_LOGIN_PATH_DEFAULT = '/media/services/rest/login.xml';
+  const EMBRIDGE_LOGIN_PATH_DEFAULT = 'media/services/rest/login.xml';
 
   /**
    * Config Factory.
@@ -56,8 +58,41 @@ class EnterMediaDbClient implements EnterMediaDbClientInterface {
   /**
    * {@inheritdoc}
    */
-  public function initRequest($path = '') {
+  public function login() {
+    $config = $this->configFactory->get('embridge.settings');
+    $query_params = [
+      'catalogid' => 'media',
+      'accountname' => $config->get('username'),
+      'password' => $config->get('password'),
+    ];
+    $query = UrlHelper::buildQuery($query_params);
+    $login_path = self::EMBRIDGE_LOGIN_PATH_DEFAULT . '?' . $query;
 
+    $request = $this->initRequest($login_path);
+    try {
+      $response = $this->httpClient->send($request);
+    }
+    catch (RequestException $e) {
+      $response = $e->getResponse();
+      if ($response === NULL) {
+        throw new \Exception('Error connecting to EMDB backend');
+      }
+      if ($response->getStatusCode() == 403) {
+        throw new \Exception('Failed to authenticate with EMDB, please check your settings.');
+      }
+    }
+    if ($response->getStatusCode() != '200') {
+      throw new \Exception('An unexpected response was returned from the Entity Pilot backend');
+    }
+    $body = (string) $response->getBody();
+
+    return TRUE;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function initRequest($path = '') {
     $settings = $this->configFactory->get('embridge.settings');
     $uri = $settings->get('uri');
     $port = $settings->get('port');
