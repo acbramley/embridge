d1cbe26d72aad9ce65b484304f58f14eca7b38c4
Adam Bramley <adamcbramley@gmail.com>
2016-02-02 17:15:40 +1100

    Swapped out file_save_upload with big copy-paste and munge to point at the eventual upload function from our client service.

--- src/Element/EmbridgeAsset.php
+++ src/Element/EmbridgeAsset.php
@@ -10,7 +10,10 @@ use Drupal\Component\Utility\Html;
 use Drupal\Core\Form\FormStateInterface;
 use Drupal\Core\Render\Element\FormElement;
 use Drupal\Core\Url;
+use Drupal\embridge\EmbridgeAssetEntityInterface;
+use Drupal\embridge\EnterMediaDbClientInterface;
 use Drupal\embridge\Entity\EmbridgeAssetEntity;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
 
 /**
  * Provides an AJAX/progress aware widget for uploading and saving a file.
@@ -363,7 +366,7 @@ class EmbridgeAsset extends FormElement {
     $files_uploaded = $element['#multiple'] && count(array_filter($file_upload)) > 0;
     $files_uploaded |= !$element['#multiple'] && !empty($file_upload);
     if ($files_uploaded) {
-      if (!$files = file_save_upload($upload_name, $element['#upload_validators'], $destination)) {
+      if (!$files = self::saveUpload($upload_name, $element['#upload_validators'], $destination)) {
         \Drupal::logger('file')->notice('The file upload failed. %upload', array('%upload' => $upload_name));
         $form_state->setError($element, t('Files in the @name field were unable to be uploaded.', array('@name' => $element['#title'])));
         return array();
@@ -377,6 +380,259 @@ class EmbridgeAsset extends FormElement {
     }
 
     return array();
+  }
+
+  /**
+   * Saves file uploads to a new location.
+   *
+   * The files will be added to the {file_managed} table as temporary files.
+   * Temporary files are periodically cleaned. Use the 'file.usage' service to
+   * register the usage of the file which will automatically mark it as permanent.
+   *
+   * @param string $form_field_name
+   *   A string that is the associative array key of the upload form element in
+   *   the form array.
+   * @param array $validators
+   *   An optional, associative array of callback functions used to validate the
+   *   file. See file_validate() for a full discussion of the array format.
+   *   If no extension validator is provided it will default to a limited safe
+   *   list of extensions which is as follows: "jpg jpeg gif png txt
+   *   doc xls pdf ppt pps odt ods odp". To allow all extensions you must
+   *   explicitly set the 'file_validate_extensions' validator to an empty array
+   *   (Beware: this is not safe and should only be allowed for trusted users, if
+   *   at all).
+   * @param string|bool $destination
+   *   A string containing the URI that the file should be copied to. This must
+   *   be a stream wrapper URI. If this value is omitted, Drupal's temporary
+   *   files scheme will be used ("temporary://").
+   * @param int $delta
+   *   Delta of the file to save or NULL to save all files. Defaults to NULL.
+   * @param int $replace
+   *   Replace behavior when the destination file already exists:
+   *   - FILE_EXISTS_REPLACE: Replace the existing file.
+   *   - FILE_EXISTS_RENAME: Append _{incrementing number} until the filename is
+   *     unique.
+   *   - FILE_EXISTS_ERROR: Do nothing and return FALSE.
+   *
+   * @return array
+   *   Function returns array of files or a single file object if $delta
+   *   != NULL. Each file object contains the file information if the
+   *   upload succeeded or FALSE in the event of an error. Function
+   *   returns NULL if no file was uploaded.
+   *
+   *   The documentation for the "File interface" group, which you can find under
+   *   Related topics, or the header at the top of this file, documents the
+   *   components of a file entity. In addition to the standard components,
+   *   this function adds:
+   *   - source: Path to the file before it is moved.
+   *   - destination: Path to the file after it is moved (same as 'uri').
+   */
+  public static function saveUpload($form_field_name, $validators = array(), $destination = FALSE, $delta = NULL, $replace = FILE_EXISTS_RENAME) {
+    $user = \Drupal::currentUser();
+    static $upload_cache;
+
+    $file_upload = \Drupal::request()->files->get("files[$form_field_name]", NULL, TRUE);
+    // Make sure there's an upload to process.
+    if (empty($file_upload)) {
+      return NULL;
+    }
+
+    // Return cached objects without processing since the file will have
+    // already been processed and the paths in $_FILES will be invalid.
+    if (isset($upload_cache[$form_field_name])) {
+      if (isset($delta)) {
+        return $upload_cache[$form_field_name][$delta];
+      }
+      return $upload_cache[$form_field_name];
+    }
+
+    // Prepare uploaded files info. Representation is slightly different
+    // for multiple uploads and we fix that here.
+    /** @var UploadedFile[] $uploaded_files */
+    $uploaded_files = $file_upload;
+    if (!is_array($file_upload)) {
+      $uploaded_files = array($file_upload);
+    }
+
+    $files = array();
+    foreach ($uploaded_files as $i => $file_info) {
+      // Check for file upload errors and return FALSE for this file if a lower
+      // level system error occurred. For a complete list of errors:
+      // See http://php.net/manual/features.file-upload.errors.php.
+      switch ($file_info->getError()) {
+        case UPLOAD_ERR_INI_SIZE:
+        case UPLOAD_ERR_FORM_SIZE:
+          drupal_set_message(t('The file %file could not be saved because it exceeds %maxsize, the maximum allowed size for uploads.', array('%file' => $file_info->getFilename(), '%maxsize' => format_size(file_upload_max_size()))), 'error');
+          $files[$i] = FALSE;
+          continue;
+
+        case UPLOAD_ERR_PARTIAL:
+        case UPLOAD_ERR_NO_FILE:
+          drupal_set_message(t('The file %file could not be saved because the upload did not complete.', array('%file' => $file_info->getFilename())), 'error');
+          $files[$i] = FALSE;
+          continue;
+
+        case UPLOAD_ERR_OK:
+          // Final check that this is a valid upload, if it isn't, use the
+          // default error handler.
+          if (is_uploaded_file($file_info->getRealPath())) {
+            break;
+          }
+
+        // Unknown error
+        default:
+          drupal_set_message(t('The file %file could not be saved. An unknown error has occurred.', array('%file' => $file_info->getFilename())), 'error');
+          $files[$i] = FALSE;
+          continue;
+
+      }
+      // Begin building file entity.
+      $values = array(
+        'uid' => $user->id(),
+        'filename' => $file_info->getClientOriginalName(),
+        'uri' => $file_info->getRealPath(),
+        'filesize' => $file_info->getSize(),
+      );
+      $values['filemime'] = \Drupal::service('file.mime_type.guesser')->guess($values['filename']);
+
+      // Create our Embridge Entity.
+      /** @var EmbridgeAssetEntityInterface $file */
+      $file = EmbridgeAssetEntity::create($values);
+
+      $extensions = '';
+      if (isset($validators['file_validate_extensions'])) {
+        if (isset($validators['file_validate_extensions'][0])) {
+          // Build the list of non-munged extensions if the caller provided them.
+          $extensions = $validators['file_validate_extensions'][0];
+        }
+        else {
+          // If 'file_validate_extensions' is set and the list is empty then the
+          // caller wants to allow any extension. In this case we have to remove the
+          // validator or else it will reject all extensions.
+          unset($validators['file_validate_extensions']);
+        }
+      }
+      else {
+        // No validator was provided, so add one using the default list.
+        // Build a default non-munged safe list for file_munge_filename().
+        $extensions = 'jpg jpeg gif png txt doc xls pdf ppt pps odt ods odp';
+        $validators['file_validate_extensions'] = array();
+        $validators['file_validate_extensions'][0] = $extensions;
+      }
+
+      if (!empty($extensions)) {
+        // Munge the filename to protect against possible malicious extension
+        // hiding within an unknown file type (ie: filename.html.foo).
+        $file->setFilename(file_munge_filename($file->getFilename(), $extensions));
+      }
+
+      // Rename potentially executable files, to help prevent exploits (i.e. will
+      // rename filename.php.foo and filename.php to filename.php.foo.txt and
+      // filename.php.txt, respectively). Don't rename if 'allow_insecure_uploads'
+      // evaluates to TRUE.
+      if (!\Drupal::config('system.file')->get('allow_insecure_uploads') && preg_match('/\.(php|pl|py|cgi|asp|js)(\.|$)/i', $file->getFilename()) && (substr($file->getFilename(), -4) != '.txt')) {
+        $file->setMimeType('text/plain');
+        // The destination filename will also later be used to create the URI.
+        $file->setFilename($file->getFilename() . '.txt');
+        // The .txt extension may not be in the allowed list of extensions. We have
+        // to add it here or else the file upload will fail.
+        if (!empty($extensions)) {
+          $validators['file_validate_extensions'][0] .= ' txt';
+          drupal_set_message(t('For security reasons, your upload has been renamed to %filename.', array('%filename' => $file->getFilename())));
+        }
+      }
+
+      // If the destination is not provided, use the temporary directory.
+      if (empty($destination)) {
+        $destination = 'temporary://';
+      }
+
+      // Assert that the destination contains a valid stream.
+      $destination_scheme = file_uri_scheme($destination);
+      if (!file_stream_wrapper_valid_scheme($destination_scheme)) {
+        drupal_set_message(t('The file could not be uploaded because the destination %destination is invalid.', array('%destination' => $destination)), 'error');
+        $files[$i] = FALSE;
+        continue;
+      }
+
+      $file->source = $form_field_name;
+      // A file URI may already have a trailing slash or look like "public://".
+      if (substr($destination, -1) != '/') {
+        $destination .= '/';
+      }
+      $file->destination = file_destination($destination . $file->getFilename(), $replace);
+      // If file_destination() returns FALSE then $replace === FILE_EXISTS_ERROR and
+      // there's an existing file so we need to bail.
+      if ($file->destination === FALSE) {
+        drupal_set_message(t('The file %source could not be uploaded because a file by that name already exists in the destination %directory.', array('%source' => $form_field_name, '%directory' => $destination)), 'error');
+        $files[$i] = FALSE;
+        continue;
+      }
+
+      // Add in our check of the file name length.
+      $validators['file_validate_name_length'] = array();
+
+      // Call the validation functions specified by this function's caller.
+      // TODO: Implement this for Embridge Assets.
+      // $errors = file_validate($file, $validators);
+
+      // Check for errors.
+      if (!empty($errors)) {
+        $message = array(
+          'error' => array(
+            '#markup' => t('The specified file %name could not be uploaded.', array('%name' => $file->getFilename())),
+          ),
+          'item_list' => array(
+            '#theme' => 'item_list',
+            '#items' => $errors,
+          ),
+        );
+        // @todo Add support for render arrays in drupal_set_message()? See
+        //  https://www.drupal.org/node/2505497.
+        drupal_set_message(\Drupal::service('renderer')->renderPlain($message), 'error');
+        $files[$i] = FALSE;
+        continue;
+      }
+
+      // Move uploaded files from PHP's upload_tmp_dir to Drupal's temporary
+      // directory. This overcomes open_basedir restrictions for future file
+      // operations.
+      $file->setSourcePath($file->destination);
+      if (!drupal_move_uploaded_file($file_info->getRealPath(), $file->getSourcePath())) {
+        drupal_set_message(t('File upload error. Could not move uploaded file.'), 'error');
+        \Drupal::logger('file')->notice('Upload error. Could not move uploaded file %file to destination %destination.', array('%file' => $file->getFilename(), '%destination' => $file->getFileUri()));
+        $files[$i] = FALSE;
+        continue;
+      }
+
+      // Set the permissions on the new file.
+      drupal_chmod($file->getSourcePath());
+
+      // If we are replacing an existing file re-use its database record.
+      // @todo Do not create a new entity in order to update it. See
+      //   https://www.drupal.org/node/2241865.
+      if ($replace == FILE_EXISTS_REPLACE) {
+        $existing_files = entity_load_multiple_by_properties('embridge_asset_entity', array('uri' => $file->getSourcePath()));
+        if (count($existing_files)) {
+          $existing = reset($existing_files);
+          $file->fid = $existing->id();
+          $file->setOriginalId($existing->id());
+        }
+      }
+
+      /** @var EnterMediaDbClientInterface $embridge_client */
+      $embridge_client = \Drupal::getContainer()->get('embridge.client');
+      $embridge_client->upload($file);
+
+      // If we made it this far it's safe to record this file in the database.
+      $file->save();
+      $files[$i] = $file;
+    }
+
+    // Add files to the cache.
+    $upload_cache[$form_field_name] = $files;
+
+    return isset($delta) ? $files[$delta] : $files;
   }
 
 }
