ca1ce146fb2befc33b7e29b1030fc8ea2c05d8b5
Adam Bramley <adamcbramley@gmail.com>
2016-02-03 13:55:52 +1100

    Adds validators to take our custom entities.

---
+++ src/Plugin/Validation/Constraint/EmbridgeAssetValidationConstraint.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\embridge\Plugin\Validation\Constraint\EmbridgeAssetValidationConstraint.
+ */
+
+namespace Drupal\embridge\Plugin\Validation\Constraint;
+
+use Symfony\Component\Validator\Constraint;
+
+/**
+ * Validation File constraint.
+ *
+ * @Constraint(
+ *   id = "EmbridgeAssetValidation",
+ *   label = @Translation("Embridge Asset Validation", context = "Validation")
+ * )
+ */
+class EmbridgeAssetValidationConstraint extends Constraint {
+
+}
---
+++ src/Plugin/Validation/Constraint/EmbridgeAssetValidationConstraintValidator.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\embridge\Plugin\Validation\Constraint\EmbridgeAssetValidationConstraintValidator.
+ */
+
+namespace Drupal\embridge\Plugin\Validation\Constraint;
+
+use Symfony\Component\Validator\Constraint;
+use Symfony\Component\Validator\ConstraintValidator;
+
+/**
+ * Checks that a embridge asset file referenced in a embridge file field is valid.
+ */
+class EmbridgeAssetValidationConstraintValidator extends ConstraintValidator {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function validate($value, Constraint $constraint) {
+    // Get the file to execute validators.
+    $asset = $value->get('entity')->getTarget()->getValue();
+    // Get the validators.
+    $validators = $value->getUploadValidators();
+    // Checks that a file meets the criteria specified by the validators.
+    if ($errors = embridge_file_validate($asset, $validators)) {
+      foreach ($errors as $error) {
+        $this->context->addViolation($error);
+      }
+    }
+  }
+
+}
