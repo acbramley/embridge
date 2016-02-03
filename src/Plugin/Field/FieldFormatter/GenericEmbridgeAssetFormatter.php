f86fe94ff6c8ec613753b496e52d9ff69e8bcba3
Adam Bramley <adamcbramley@gmail.com>
2016-02-03 14:22:20 +1100

    Fix up formatter.

--- src/Plugin/Field/FieldFormatter/GenericEmbridgeAssetFormatter.php
+++ src/Plugin/Field/FieldFormatter/GenericEmbridgeAssetFormatter.php
@@ -69,28 +69,19 @@ class GenericEmbridgeAssetFormatter extends EntityReferenceFormatterBase {
    * {@inheritdoc}
    */
   public function viewElements(FieldItemListInterface $items, $langcode) {
-    $elements = [];
+    $elements = array();
 
-    foreach ($items as $delta => $item) {
-      $elements[$delta] = ['#markup' => $this->viewValue($item)];
+    foreach ($this->getEntitiesToView($items, $langcode) as $delta => $asset) {
+      $elements[$delta] = array(
+        '#theme' => 'embridge_file_link',
+        '#asset' => $asset,
+        '#cache' => array(
+          'tags' => $asset->getCacheTags(),
+        ),
+      );
     }
 
     return $elements;
-  }
-
-  /**
-   * Generate the output appropriate for one field item.
-   *
-   * @param \Drupal\Core\Field\FieldItemInterface $item
-   *   One field item.
-   *
-   * @return string
-   *   The textual output generated.
-   */
-  protected function viewValue(FieldItemInterface $item) {
-    // The text value has no text format assigned to it, so the user input
-    // should equal the output, including newlines.
-    return nl2br(Html::escape($item->value));
   }
 
 }
