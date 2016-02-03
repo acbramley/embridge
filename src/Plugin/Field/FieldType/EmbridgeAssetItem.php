307dd43da4e957f2285258e360b47fd4ab175243
Adam Bramley <adamcbramley@gmail.com>
2016-02-04 09:21:51 +1100

    Fix validators in EmbridgeAssetItem.

--- src/Plugin/Field/FieldType/EmbridgeAssetItem.php
+++ src/Plugin/Field/FieldType/EmbridgeAssetItem.php
@@ -118,11 +118,11 @@ class EmbridgeAssetItem extends FileItem {
     }
 
     // There is always a file size limit due to the PHP server limit.
-    $validators['embridge_file_validate_size'] = array($max_filesize);
+    $validators['embridge_asset_validate_file_size'] = array($max_filesize);
 
     // Add the extension check if necessary.
     if (!empty($settings['file_extensions'])) {
-      $validators['embridge_file_validate_extensions'] = array($settings['file_extensions']);
+      $validators['embridge_asset_validate_file_extensions'] = array($settings['file_extensions']);
     }
 
     return $validators;
