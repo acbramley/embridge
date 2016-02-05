b165ce29946b75d19c12724e8e2faab56924ef57
Adam Bramley <adamcbramley@gmail.com>
2016-02-05 11:29:52 +1100

    Refactor to use the JSON API.

--- src/EnterMediaDbClient.php
+++ src/EnterMediaDbClient.php
@@ -8,7 +8,6 @@ namespace Drupal\embridge;
 
 
 use Drupal\Component\Serialization\SerializationInterface;
-use Drupal\Component\Utility\UrlHelper;
 use Drupal\Core\Config\ConfigFactoryInterface;
 use Drupal\Core\File\FileSystem;
 use GuzzleHttp\ClientInterface;
@@ -17,7 +16,7 @@ use GuzzleHttp\Exception\RequestException;
 
 class EnterMediaDbClient implements EnterMediaDbClientInterface {
 
-  const EMBRIDGE_LOGIN_PATH_DEFAULT = 'media/services/rest/login.xml';
+  const EMBRIDGE_LOGIN_PATH_DEFAULT = 'mediadb/services/authentication/login';
   const EMBRIDGE_UPLOAD_PATH_DEFAULT = 'media/services/rest/upload.xml';
 
   /**
@@ -93,8 +92,8 @@ class EnterMediaDbClient implements EnterMediaDbClientInterface {
    * @param string $method
    *   The method to use on the request, defaults to POST.
    *
-   * @return \Psr\Http\Message\ResponseInterface
-   *   The response from the server.
+   * @return []
+   *   An array of the body of the response from the server.
    *
    * @throws \Exception
    *   When something goes wrong with the request (i.e 403).
@@ -103,13 +102,14 @@ class EnterMediaDbClient implements EnterMediaDbClientInterface {
     $settings = $this->configFactory->get('embridge.settings');
     $uri = $settings->get('uri');
     $port = $settings->get('port');
-    $uri = sprintf('%s:%s/%s', $uri, $port, $path);
+    $uri = sprintf('%s/%s', $uri, $path);
     $options = [
       'timeout' => 5,
       'cookies' => $this->cookieJar,
     ];
     if (!empty($body)) {
-      $options['form_params'] = $body;
+      $options['_body_as_string'] = TRUE;
+      $options['body'] = $this->jsonEncoder->encode($body);
     }
 
     try {
@@ -124,11 +124,18 @@ class EnterMediaDbClient implements EnterMediaDbClientInterface {
         throw new \Exception('Failed to authenticate with EMDB, please check your settings.');
       }
     }
+
     if ($response->getStatusCode() != '200') {
       throw new \Exception('An unexpected response was returned from the Entity Pilot backend');
     }
 
-    return $response;
+    $body = $this->jsonEncoder->decode((string) $response->getBody());
+
+    if (empty($body['response']['status']) || $body['response']['status'] != 'ok') {
+      throw new \Exception(sprintf('The request to EnterMedia failed.'));
+    }
+
+    return $body;
   }
 
   /**
@@ -140,25 +147,19 @@ class EnterMediaDbClient implements EnterMediaDbClientInterface {
     }
 
     $config = $this->configFactory->get('embridge.settings');
-    $query_params = [
-      'catalogid' => 'media',
-      'accountname' => $config->get('username'),
+    $body = [
+      'id' => $config->get('username'),
       'password' => $config->get('password'),
     ];
-    $query = UrlHelper::buildQuery($query_params);
-    $login_path = self::EMBRIDGE_LOGIN_PATH_DEFAULT . '?' . $query;
 
-    $response = $this->doRequest($login_path);
+    $body = $this->doRequest(self::EMBRIDGE_LOGIN_PATH_DEFAULT, $body);
 
-    $body = (string) $response->getBody();
-    $xml_obj = simplexml_load_string($body);
-    $xml_arr = (array) $xml_obj;
-    if (!empty($xml_arr['@attributes']['stat']) && $xml_arr['@attributes']['stat'] == 'ok') {
-      $this->loggedIn = TRUE;
-      return TRUE;
+    if (!empty($body['results']['status']) && $body['results']['status'] != 'invalidlogin') {
+      return FALSE;
     }
 
-    return FALSE;
+    $this->loggedIn = TRUE;
+    return TRUE;
   }
 
   /**
