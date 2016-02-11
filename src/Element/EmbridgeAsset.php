2bc7149d6db71ed6c67e081d6aec9ef30c522fc7
Adam Bramley <adamcbramley@gmail.com>
2016-02-11 14:48:20 +1100

    Add field name to link

--- src/Element/EmbridgeAsset.php
+++ src/Element/EmbridgeAsset.php
@@ -371,7 +371,7 @@ class EmbridgeAsset extends FormElement {
 
     // Build link for dialog.
     if (\Drupal::currentUser()->hasPermission('search embridge assets')) {
-      $link_url = Url::fromRoute('embridge.search.modal', ['delta' => $element['#delta']]);
+      $link_url = Url::fromRoute('embridge.search.modal', ['field_name' => $element['#field_name'], 'delta' => $element['#delta']]);
       $link_url->setOptions(array(
           'attributes' => array(
             'class' => array('use-ajax', 'button'),
