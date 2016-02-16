5bb791a9c467d0a8136d05ce831b7d0982dc4682
Adam Bramley <adam.bramley@justice.vic.gov.au>
2016-02-16 16:08:23 +1100

    Fixes phpcs for EnterMediaAssetHelperInterface

--- src/EnterMediaAssetHelperInterface.php
+++ src/EnterMediaAssetHelperInterface.php
@@ -16,13 +16,14 @@ interface EnterMediaAssetHelperInterface {
    * Returns a url for an Embridge Asset entity using a conversion.
    *
    * @param \Drupal\embridge\EmbridgeAssetEntityInterface $asset
-   *   The embridge asset entity
+   *   The embridge asset entity.
    * @param string $application_id
    *   The application id for the catalog this asset resides in.
    * @param string $conversion
    *   The conversion to get the url for.
    *
    * @return string
+   *   The fully qualified url to the asset.
    */
   public function getAssetConversionUrl(EmbridgeAssetEntityInterface $asset, $application_id, $conversion);
 
