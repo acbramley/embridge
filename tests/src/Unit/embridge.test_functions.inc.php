81fa55868f52016fead9e793a8dd1262a16dfe3f
Adam Bramley <adam.bramley@justice.vic.gov.au>
2016-03-09 14:34:20 +1100

    Unindent namespace in test functions.

--- tests/src/Unit/embridge.test_functions.inc.php
+++ tests/src/Unit/embridge.test_functions.inc.php
@@ -4,21 +4,20 @@
  * File of testing functions.
  */
 
-namespace Drupal\embridge {
+namespace Drupal\embridge;
 
-  define('MOCK_TIMESTAMP', 1456192161);
+define('MOCK_TIMESTAMP', 1456192161);
 
-  // Mock time() for our namespace.
-  if (!function_exists('\Drupal\embridge\time')) {
+// Mock time() for our namespace.
+if (!function_exists('\Drupal\embridge\time')) {
 
-    /**
-     * Mock instance of the time() function.
-     *
-     * @return int
-     *   The same timestamp always.
-     */
-    function time() {
-      return MOCK_TIMESTAMP;
-    }
+  /**
+   * Mock instance of the time() function.
+   *
+   * @return int
+   *   The same timestamp always.
+   */
+  function time() {
+    return MOCK_TIMESTAMP;
   }
 }
