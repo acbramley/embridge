0f149f0d366952e023ded8dc5d0a69569357debf
Adam Bramley <adam.bramley@justice.vic.gov.au>
2016-03-09 15:57:41 +1100

    Add submitForm tests

--- modules/embridge_ckeditor/tests/src/Unit/EmbridgeCkeditorImageDialogTest.php
+++ modules/embridge_ckeditor/tests/src/Unit/EmbridgeCkeditorImageDialogTest.php
@@ -15,6 +15,7 @@ use Drupal\Core\Form\FormState;
 use Drupal\editor\Entity\Editor;
 use Drupal\embridge\EnterMediaAssetHelper;
 use Drupal\embridge\Entity\EmbridgeAssetEntity;
+use Drupal\embridge\Entity\EmbridgeCatalog;
 use Drupal\embridge_ckeditor\Form\EmbridgeCkeditorImageDialog;
 use Drupal\filter\Entity\FilterFormat;
 use Drupal\Tests\Core\Form\FormTestBase;
@@ -105,6 +106,19 @@ class EmbridgeCkeditorImageDialogTest extends FormTestBase {
     $this->mockFilter = $this->getMockBuilder(FilterFormat::class)
       ->disableOriginalConstructor()
       ->getMock();
+
+    // Initialise our form.
+    $this->form = new EmbridgeCkeditorImageDialog(
+      $this->entityTypeManager,
+      $this->entityRepository,
+      $this->assetHelper
+    );
+  }
+
+  /**
+   * Sets up mocks for buildForm() calls.
+   */
+  private function setUpBuildForm() {
     $this->mockFilter->expects($this->once())
       ->method('id')
       ->willReturn(self::MOCK_FILTER_ID);
@@ -141,13 +155,6 @@ class EmbridgeCkeditorImageDialogTest extends FormTestBase {
       ->method('getStorage')
       ->with('editor')
       ->willReturn($mock_storage);
-
-    // Initialise our form.
-    $this->form = new EmbridgeCkeditorImageDialog(
-      $this->entityTypeManager,
-      $this->entityRepository,
-      $this->assetHelper
-    );
   }
 
   /**
@@ -167,6 +174,7 @@ class EmbridgeCkeditorImageDialogTest extends FormTestBase {
    * @test
    */
   public function buildFormWithEmptyImageElementReturnsExpectedForm() {
+    $this->setUpBuildForm();
     $form = [];
     // This is what the form recevies from ckeditor.
     $user_input['editor_object'] = [
@@ -206,6 +214,7 @@ class EmbridgeCkeditorImageDialogTest extends FormTestBase {
    * @test
    */
   public function buildFormWithExistingImageElementReturnsExpectedForm() {
+    $this->setUpBuildForm();
     $form = [];
     // This is what the form recevies from ckeditor.
     $user_input['editor_object'] = [
@@ -249,6 +258,104 @@ class EmbridgeCkeditorImageDialogTest extends FormTestBase {
 
     // Test form builds correctly.
     $this->assertJsonStringEqualsJsonString($expected, Json::encode($actual));
+  }
+
+  /**
+   * Test submitForm with empty fid.
+   *
+   * @covers ::submitForm
+   *
+   * @test
+   */
+  public function submitFormWithEmptyFidDoesNotLoadEntities() {
+    $form = [];
+    $form_state = new FormState();
+
+    $response = $this->form->submitForm($form, $form_state);
+
+    $this->assertInstanceOf('\Drupal\Core\Ajax\AjaxResponse', $response);
+    $commands = $response->getCommands();
+    $this->assertNotEmpty($commands);
+    $this->assertCount(2, $commands);
+    $this->assertEquals('editorDialogSave', $commands[0]['command']);
+    $this->assertEmpty($commands[0]['values']);
+    $this->assertEquals('closeDialog', $commands[1]['command']);
+  }
+
+
+  /**
+   * Test submitForm with an fid, ensures values are set correctly.
+   *
+   * @covers ::submitForm
+   *
+   * @test
+   */
+  public function submitFormWithAndFidLoadsEntitiesAndSetsFormStateValues() {
+    $form = [];
+    $catalog_id = 'test_catalog';
+    $app_id = 'test_application_id';
+    $source_url = 'www.example.com/test_application_id/test.jpg';
+
+    $form['asset']['#catalog_id'] = $catalog_id;
+    $form_state = new FormState();
+    $intial_values = [
+      'asset' => [self::MOCK_ASSET_ID],
+    ];
+    $form_state->setValues($intial_values);
+
+    $mock_asset = $this->getMockBuilder(EmbridgeAssetEntity::class)
+      ->disableOriginalConstructor()
+      ->getMock();
+    $mock_asset->expects($this->once())
+      ->method('uuid')
+      ->willReturn(self::MOCK_ASSET_UUID);
+
+    $mock_asset_storage = $this->getMockBuilder(EntityStorageInterface::class)
+      ->disableOriginalConstructor()
+      ->getMock();
+    $mock_asset_storage->expects($this->once())
+      ->method('load')
+      ->with(self::MOCK_ASSET_ID)
+      ->willReturn($mock_asset);
+
+    $mock_catalog = $this->getMockBuilder(EmbridgeCatalog::class)
+      ->disableOriginalConstructor()
+      ->getMock();
+    $mock_catalog->expects($this->once())
+      ->method('getApplicationId')
+      ->willReturn($app_id);
+
+    $mock_catalog_storage = $this->getMockBuilder(EntityStorageInterface::class)
+      ->disableOriginalConstructor()
+      ->getMock();
+    $mock_catalog_storage->expects($this->once())
+      ->method('load')
+      ->with($catalog_id)
+      ->willReturn($mock_catalog);
+
+    $this->assetHelper->expects($this->once())
+      ->method('getAssetConversionUrl')
+      ->with($mock_asset, $app_id, 'thumb')
+      ->willReturn($source_url);
+
+    $this->entityTypeManager->expects($this->exactly(2))
+      ->method('getStorage')
+      ->will($this->returnValueMap([
+        ['embridge_asset_entity', $mock_asset_storage],
+        ['embridge_catalog', $mock_catalog_storage],
+      ]));
+
+    $this->form->submitForm($form, $form_state);
+    $expected_values = [
+      'attributes' => [
+        'src' => $source_url,
+        'data-entity-uuid' => self::MOCK_ASSET_UUID,
+        'data-entity-type' => 'embridge_asset_entity',
+      ],
+    ] + $intial_values;
+
+    $actual_values = $form_state->getValues();
+    $this->assertEquals($expected_values, $actual_values);
   }
 
 }
