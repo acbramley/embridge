a0d43fc56adf46016b2beab43f674f8ff3ca254b
Adam Bramley <adam.bramley@justice.vic.gov.au>
2016-03-17 10:42:57 +1100

    Swap to be a Contextual Plugin as we are now extending DrupalImage.

--- modules/embridge_ckeditor/src/Plugin/CKEditorPlugin/EmbridgeImage.php
+++ modules/embridge_ckeditor/src/Plugin/CKEditorPlugin/EmbridgeImage.php
@@ -7,8 +7,10 @@
 
 namespace Drupal\embridge_ckeditor\Plugin\CKEditorPlugin;
 
-use Drupal\ckeditor\CKEditorPluginBase;
 use Drupal\ckeditor\CKEditorPluginConfigurableInterface;
+use Drupal\ckeditor\CKEditorPluginContextualInterface;
+use Drupal\ckeditor\CKEditorPluginInterface;
+use Drupal\Component\Plugin\PluginBase;
 use Drupal\Core\Form\FormStateInterface;
 use Drupal\editor\Entity\Editor;
 use Drupal\embridge\Entity\EmbridgeCatalog;
@@ -22,7 +24,21 @@ use Drupal\embridge\Entity\EmbridgeCatalog;
  *   module = "embridge_ckeditor"
  * )
  */
-class EmbridgeImage extends CKEditorPluginBase implements CKEditorPluginConfigurableInterface {
+class EmbridgeImage extends PluginBase implements CKEditorPluginInterface, CKEditorPluginConfigurableInterface, CKEditorPluginContextualInterface {
+  /**
+   * {@inheritdoc}
+   */
+  public function isInternal() {
+    return FALSE;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getDependencies(Editor $editor) {
+    return [];
+  }
+
   /**
    * {@inheritdoc}
    */
@@ -34,31 +50,14 @@ class EmbridgeImage extends CKEditorPluginBase implements CKEditorPluginConfigur
    * {@inheritdoc}
    */
   public function getLibraries(Editor $editor) {
-    return array(
-      'core/drupal.ajax',
-    );
+    return [];
   }
 
   /**
    * {@inheritdoc}
    */
   public function getConfig(Editor $editor) {
-    return array(
-      'embridgeImage_dialogTitleAdd' => t('Insert Image'),
-      'embridgeImage_dialogTitleEdit' => t('Edit Image'),
-    );
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function getButtons() {
-    return array(
-      'EmbridgeImage' => [
-        'label' => t('EMBridge Image'),
-        'image' => drupal_get_path('module', 'embridge_ckeditor') . '/js/plugins/embridgeimage/image.png',
-      ],
-    );
+    return [];
   }
 
   /**
@@ -73,28 +72,45 @@ class EmbridgeImage extends CKEditorPluginBase implements CKEditorPluginConfigur
       'directory' => 'embridge-inline-images',
       'max_size' => '',
       'catalog' => '',
+      'enabled' => FALSE,
     ];
     $sub_form = [];
 
     $sub_form['warning'] = [
-      '#markup' => $this->t("<strong>Warning: This plugin is not compatible with core's image dialog.</strong>"),
+      '#markup' => t("<strong>Warning: This plugin overrides the standard drupal image plugin.</strong>"),
     ];
+    $sub_form['enabled'] = [
+      '#type' => 'checkbox',
+      '#default_value' => $plugin_settings['enabled'],
+      '#title' => t('Enable plugin'),
+      '#description' => t("Tick this to connect the image dialog with EMDB."),
+      '#attributes' => array(
+        'data-editor-embridge-upload' => 'enabled',
+      ),
+    ];
+    $show_if_plugin_uploads_enabled = array(
+      'visible' => array(
+        ':input[data-editor-embridge-upload="enabled"]' => array('checked' => TRUE),
+      ),
+    );
     $sub_form['directory'] = [
       '#type' => 'textfield',
       '#default_value' => $plugin_settings['directory'],
-      '#title' => $this->t('Upload directory'),
-      '#description' => $this->t("A temporary directory to upload images into before sending to EMDB."),
+      '#title' => t('Upload directory'),
+      '#description' => t("A temporary directory to upload images into before sending to EMDB."),
+      '#states' => $show_if_plugin_uploads_enabled,
     ];
     $default_max_size = format_size(file_upload_max_size());
     $sub_form['max_size'] = [
       '#type' => 'textfield',
       '#default_value' => $plugin_settings['max_size'],
-      '#title' => $this->t('Maximum file size'),
-      '#description' => $this->t('If this is left empty, then the file size will be limited by the PHP maximum upload size of @size.', ['@size' => $default_max_size]
+      '#title' => t('Maximum file size'),
+      '#description' => t('If this is left empty, then the file size will be limited by the PHP maximum upload size of @size.', ['@size' => $default_max_size]
       ),
       '#maxlength' => 20,
       '#size' => 10,
       '#placeholder' => $default_max_size,
+      '#states' => $show_if_plugin_uploads_enabled,
     ];
     /** @var EmbridgeCatalog[] $entities */
     $entities = EmbridgeCatalog::loadMultiple();
@@ -109,9 +125,10 @@ class EmbridgeImage extends CKEditorPluginBase implements CKEditorPluginConfigur
       '#title' => t('Catalog'),
       '#default_value' => $plugin_settings['catalog'],
       '#options' => $options,
-      '#description' => $this->t("Select the Catalog to source media from for this field."),
+      '#description' => t("Select the Catalog to source media from for this field."),
       '#required' => TRUE,
       '#weight' => 6,
+      '#states' => $show_if_plugin_uploads_enabled,
     ];
 
     $sub_form['#attached']['library'][] = 'embridge_ckeditor/drupal.embridge_ckeditor.embridgeimage.admin';
@@ -119,6 +136,26 @@ class EmbridgeImage extends CKEditorPluginBase implements CKEditorPluginConfigur
     $form['embridge_image_upload'] = $sub_form;
 
     return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function isEnabled(Editor $editor) {
+    $enabled = FALSE;
+
+    if (!$editor->hasAssociatedFilterFormat()) {
+      return $enabled;
+    }
+
+    $editor_settings = $editor->getSettings();
+    $plugin_settings = !empty($editor_settings['plugins']['embridgeimage']['embridge_image_upload']) ? $editor_settings['plugins']['embridgeimage']['embridge_image_upload'] : [];
+
+    if (!empty($plugin_settings['enabled']) && $plugin_settings['enabled']) {
+      $enabled = TRUE;
+    }
+
+    return $enabled;
   }
 
 }
