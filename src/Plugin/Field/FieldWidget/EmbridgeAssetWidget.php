8056ece3acd5fc605ccb7bcd8ab405760ff550e4
Simon Hobbs <simon.hobbs@justice.vic.gov.au>
2016-03-18 08:43:41 +1100

    Pull the library id from the field setting.

--- src/Plugin/Field/FieldWidget/EmbridgeAssetWidget.php
+++ src/Plugin/Field/FieldWidget/EmbridgeAssetWidget.php
@@ -97,7 +97,7 @@ class EmbridgeAssetWidget extends FileWidget {
       '#description_field' => $field_settings['description_field'],
       '#cardinality' => $cardinality,
       '#catalog_id' => $field_settings['catalog_id'],
-      '#library_id' => 101,
+      '#library_id' => $field_settings['library_id'],
     );
 
     $element['#weight'] = $delta;
